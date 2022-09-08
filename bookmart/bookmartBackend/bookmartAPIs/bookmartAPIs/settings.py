a60397ef592091506823702eace8bb9ad827653b
karamjeetsingh.coderK <karamjeetsingh.de@gmail.com>
2022-09-08 11:16:43 +0200

    add OPEN API Swagger Configuration

--- bookmart/bookmartBackend/bookmartAPIs/bookmartAPIs/settings.py
+++ bookmart/bookmartBackend/bookmartAPIs/bookmartAPIs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "rest_framework_swagger",
     "corsheaders",
     "book",
     "order",
@@ -69,6 +70,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries' : {
+                'staticfiles': 'django.templatetags.static', 
+            }
         },
     },
 ]
@@ -85,7 +89,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -124,9 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/images/'
+
 STATICFILES_DIRS =[
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_ROOT = 'static/images'
--- bookmart/bookmartBackend/bookmartAPIs/bookmartAPIs/urls.py
+++ bookmart/bookmartBackend/bookmartAPIs/bookmartAPIs/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='bookmart API')
 
 urlpatterns = [
+    path("", schema_view),
     path("admin/", admin.site.urls),
     path("api/users/", include("user.urls")),
     
